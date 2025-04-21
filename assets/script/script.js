70f5cca6ecab9ea1869452f3885b4db7aff210e7
kalgo4444 <abuxyziabd@gmail.com>
2025-04-21 13:43:00 +0500

    add js masala

---
+++ assets/script/script.js
@@ -0,0 +1,183 @@
+{
+    // case 1
+    // let weekNum = 2, r;
+    // switch(weekNum){
+    //     case 1:
+    //         r = "dushanbi"
+    //         break;
+    //     case 2:
+    //         r = "seshanbi"
+    //         break;
+    //     case 3:
+    //         r = "chorshanbi"
+    //         break;
+    //     case 4:
+    //         r = "payshanbi"
+    //         break;
+    //     case 5:
+    //         r = "juma"
+    //         break;
+    //     case 6:
+    //         r = "shanbi"
+    //         break;
+    //     case 7:
+    //         r = "yakshanbi"
+    //         break;
+    //         default:
+    //             r = "error"
+    // }
+    // console.log(r);
+}
+{
+    // case 2
+    // let k = 0;
+    // let r;
+    // switch(k){
+    //     case 1:
+    //         r = "yomon"
+    //         break;
+    //     case 2:
+    //         r = "qoniqarsiz"
+    //         break;
+    //     case 1:
+    //         r = "qoniqarli"
+    //         break;
+    //     case 1:
+    //         r = "yaxshi"
+    //         break;
+    //     case 1:
+    //         r = "a'lo"
+    //         break;
+    //     default:
+    //         r = "xato"
+    // }
+    // console.log(r);
+}
+{       
+    // case 3
+    // let monthNumber = 2;
+    // let r;
+    // switch(monthNumber){
+    //     case 12:
+    //     case 1:
+    //     case 2:
+    //         r = "qish"
+    //         break;
+    //     case 3:
+    //     case 4:
+    //     case 5:
+    //         r = "bahor"
+    //         break;
+    //     case 6:
+    //     case 7:
+    //     case 8:
+    //         r = "yoz"
+    //         break;
+    //     case 9:
+    //     case 10:
+    //     case 11:
+    //         r = "kuz"
+    //         break;
+    // }
+    // console.log(r);
+}
+{
+    // case 4 
+    //1yanvar, 3mart, 5may, 7iyul, 8avgust, 10oktyabr, 12dekabr   = 31
+    //4aprel, 6iyun, 9sentyabr, 11noyabr,    = 30
+    //2fevral = 28
+    // let monthNumber = 4, r;
+    // switch(monthNumber){
+    //     case 1:
+    //     case 3:
+    //     case 5:
+    //     case 7:
+    //     case 8:
+    //     case 10:
+    //     case 12:
+    //         r = "31 kun 1 oyda ichida"
+    //         break;
+    //     case 4:
+    //     case 6:
+    //     case 9:
+    //     case 11:
+    //         r = "30 kun 1 oyda ichida"
+    //         break;
+    //     case 2:
+    //         r = "28 kun 1 oyda ichida"
+    //         break;
+    //     default:
+    //         "error"
+    // }
+    // console.log(r);
+}
+{
+    // case 5
+    // let a = 10, b = 3, r;
+    // let userChoise = 1;
+    // switch(userChoise){
+    //     case 1:
+    //         r = a + b
+    //         break;
+    //     case 2:
+    //         r = a - b
+    //         break;
+    //     case 3:
+    //         r = a * b
+    //         break;
+    //     case 4:
+    //         r = a / b
+    //         break;
+    // }
+    // console.log(r);
+}
+{
+    // case 6   
+    // let userEnterValue = 3.14; //m
+    // let userChoiseBirliklar = 1, r;
+    // switch(userChoiseBirliklar){
+    //     case 1:
+    //         r = userEnterValue * 0.1
+    //         break;
+    //     case 2:
+    //         r = userEnterValue * 1000
+    //         break;
+    //     case 3:
+    //         r = userEnterValue * 100
+    //         break;
+    //     case 4:
+    //         r = userEnterValue * 0.001
+    //         break;
+    //     case 5:
+    //         r = userEnterValue * 0.01
+    //         break;
+    //     default:
+    //         console.log("error");
+    // }
+    // console.log(r);
+}
+{
+    // case 7 
+    // let userEnterValue = 3.14; //kg
+    // let userChoiseBirliklar = 1, r;
+    // switch(userChoiseBirliklar){
+    //     case 1:
+    //         r = userEnterValue * 100
+    //         break;
+    //     case 2:
+    //         r = userEnterValue * 1_000_000
+    //         break;
+    //     case 3:
+    //         r = userEnterValue * 1_000
+    //         break;
+    //     case 4:
+    //         r = userEnterValue * 0.001
+    //         break;
+    //     case 5:
+    //         r = userEnterValue * 0.01
+    //         break;
+    //     default:
+    //         console.log("error");
+    // }
+    // console.log(r);
+}
